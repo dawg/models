d9cb4ddb4239f3cc5081074b6b01cf7d42b706a0
Amir Eldesoky <aeldesok@unb.ca>
2018-11-26 17:54:32 -0400

    removing the setup script

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
-from setuptools import find_packages
+# from distutils.core import setup
+# from setuptools import find_packages
 
-requires = ["six>=1.10.0"]
+# requires = ["six>=1.10.0"]
 
-if __name__ == "__main__":
-    setup(
-        name="vusic-models",
-        packages=find_packages(),
-        author="DAWG",
-        install_requires=requires,
-        include_package_data=True,
-    )
+# if __name__ == "__main__":
+#     setup(
+#         name="vusic-models",
+#         packages=find_packages(),
+#         author="DAWG",
+#         install_requires=requires,
+#         include_package_data=True,
+#     )

d5b263d22f8c5b65d36cfb08aa1f0ec6669c38ea
alexodonn <alexodonn@gmail.com>
2018-11-27 20:35:34 -0400

    reformatted setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(
-    name="vusic",
-    packages=find_packages(),
-    author="DAWG",
-    include_package_data=True,
-)
+setup(name="vusic", packages=find_packages(), author="DAWG", include_package_data=True)

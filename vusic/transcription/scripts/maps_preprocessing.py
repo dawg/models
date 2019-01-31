3889f5818f1098f81bffe868e091b2a1e7c56c41
desilvamatt <m7n9k@unb.ca>
2019-01-31 17:30:36 -0400

    Started building tensors for MAPS dataset

--- vusic/transcription/scripts/maps_preprocessing.py
+++ vusic/transcription/scripts/maps_preprocessing.py
@@ -6,6 +6,8 @@ import logme
 import tqdm
 import zipfile
 import boto3
+import torchaudio
+import logger
 
 from vusic.utils.downloader import Downloader
 from magenta.music import audio_io
@@ -24,34 +26,46 @@ train_dirs = [
 ]
 
 
-def generate_training_set(exclude_ids):
+def generate_training_set(unzipped_dir: str):
     """
     Desc:
         Generate the training tensors
 
     Args:
-        exclude_ids (string): Desc
+        unzipped_dir (string): Directory where unzipped data is located
     """
+    # TODO Make writer, but for now...
+    dst = os.path.expanduser("~")
+    dst = os.path.join(dst, "storage", "transcription")
+    if not os.path.isdir(dst):
+        print(f"Creating {dst}")
+        os.mkdir(dst)
+        dst = os.path.join(dst,"training")
+        os.mkdir(dst)
+
     train_file_pairs = []
     for d in train_dirs:
-        #TODO define and point to directories
-        path = os.path.join(input_dir, directory)
-        path = os.path.join(path, '*.wav')
+        # TODO define and point to directories
+        path = os.path.join(unzipped_dir, d)
+        path = os.path.join(path, "*.wav")
         wav_files = glob.glob(path)
-        
+
+        # find mid files
+        for f in wav_files:
+            base_name_root, _ = os.path.splitext(f)
+            mid_file = base_name_root + ".mid"
+            train_file_pairs.append((f, mid_file))
 
 
+    
 
 
 def main():
     downloader = Downloader.from_params(preprocess_settings["downlader"])
-    
+
     dst = preprocess_settings["pre_dst"]
 
-
-
     downloader.get_dataset()
-
 
 
 if __name__ == "__main__":

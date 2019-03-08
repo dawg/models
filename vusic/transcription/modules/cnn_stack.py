be9d422d2c0a28576d96b78d88f532cd88613bda
desilvamatt <m7n9k@unb.ca>
2019-03-08 16:38:10 -0400

    cnn stack+onset bilstm shapes complete :squirrel:

--- vusic/transcription/modules/cnn_stack.py
+++ vusic/transcription/modules/cnn_stack.py
@@ -10,10 +10,10 @@ class CnnStack(nn.Module):
         """
         Desc:
             Create implementation of a CNN stack, the (N,C,H,W) of the dataset is as follows: 
-            N = batch size  =
-            C = channels    =
-            H = height      =
-            W = width       =
+            N = batch size  = x
+            C = channels    = initially 3
+            H = height      = 626
+            W = width       = 226
 
         Args:
             debug (bool): debug mode
@@ -21,28 +21,44 @@ class CnnStack(nn.Module):
         super(CnnStack, self).__init__()
         self.device = "cuda" if not debug and torch.cuda.is_available() else "cpu"
 
-        self.inputlayer = nn.Linear(
-            in_features, out_features
-        )  # TODO, is this layer necessary?
-        self.conv1 = nn.Conv2d(inchannel, 32, 3, stride=1)  # TODO, input channels
-        self.conv2 = nn.Conv2d(inchannel, 32, 3, stride=1)  # TODO, input channels
-        self.normalization = nn.BatchNorm2d(C)  # TODO C from NCHW
+        # conv: 3*626*226   --> 32*626*226
+        self.conv1 = nn.Conv2d(3, 32, 3, stride=1)
+        # conv: 32*626*226  --> 32*626*226
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=1)
+        # norm: 32*626*226  --> 32*626*226
+        self.normalization = nn.BatchNorm2d(32)
+        # pool: 32*626*226  --> 32*626*113
         self.pooling = nn.MaxPool2d(kernel_size=(1, 2))
+        # drop: 32*626*113  --> 32*626*113
         self.dropout1 = nn.Dropout2d(p=0.25)
-        self.conv3 = nn.Conv2d(inchannel, 64, 3, stride=1)  # TODO, input channels
+        # conv: 32*626*113  --> 64*626*113
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=1)
+        # drop: 64*626*113  --> 64*626*113
         self.dropout2 = nn.Dropout2d(p=0.25)
-        self.fnn = nn.Linear(64, 512)  # TODO maybe change infeatures
+        # fnn:  64*626*113  --> 512*626*113
+        self.fnn = nn.Linear(64, 512)
+        # drop: 512*626*113 --> 512*626*113
         self.dropout3 = nn.Dropout2d(p=0.5)
 
     def forward(self, x):
-        out = self.inputlayer(x)  # TODO maybe remove layer
-        out = self.conv1(out)
+        print("In forward function")
+        print(f"Shape before conv1 {x.shape}")
+        out = self.conv1(x)
+        print(f"Shape before conv2 {out.shape}")
         out = self.conv2(out)
+        print(f"Shape before normalization {out.shape}")
         out = self.normalization(out)
+        print(f"Shape before pooling {out.shape}")
         out = self.pooling(out)
+        print(f"Shape before dropout1 {out.shape}")
         out = self.dropout1(out)
+        print(f"Shape before conv3 {out.shape}")
         out = self.conv3(out)
+        print(f"Shape before dropout2 {out.shape}")
         out = self.dropout2(out)
+        print(f"Shape before fnn {out.shape}")
         out = self.fnn(out)
+        print(f"Shape before dropout3 {out.shape}")
         out = self.dropout3(out)
+        print(f"Final shape {out.shape}")
         return out

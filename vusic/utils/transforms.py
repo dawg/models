b60536c96d047095af80546dab8ee68c70375609
alexodonn <alexodonn@gmail.com>
2019-03-19 13:14:38 -0300

    fix transform bug and add separation script

--- vusic/utils/transforms.py
+++ vusic/utils/transforms.py
@@ -5,70 +5,69 @@ from numpy.lib import stride_tricks
 
 from vusic.utils.separation_settings import training_settings
 
-sequence_length = training_settings["sequence_length"]
-context_length = training_settings["context_length"]
-batch_size = training_settings["batch_size"]
 
+def context_reshape(mix, voice, context_length, win_length):
+    """
+        Reshape based on context
+    """
+    mix = np.ascontiguousarray(mix[:, context_length:-context_length, :], dtype=np.float32)
+    mix.shape = (mix.shape[0] * mix.shape[1], win_length)
+    voice = np.ascontiguousarray(voice[:, context_length:-context_length, :], dtype=np.float32)
+    voice.shape = (voice.shape[0] * voice.shape[1], win_length)
+    
+    return mix, voice
 
 def overlap_transform(sample):
     """
         Make samples overlap by context length frames. return the transformed sample
     """
 
-    trim_frame = sample["mix"]["mg"].shape[0] % (sequence_length - context_length)
-    trim_frame -= sequence_length - context_length
+    # Fixme 
+    sequence_length = training_settings["sequence_length"]
+    context_length = training_settings["context_length"]
+    batch_size = training_settings["batch_size"]
+
+    sample['mix']['mg'] = overlap_sequences(sample['mix']['mg'], context_length, sequence_length, batch_size)
+    sample['vocals']['mg'] = overlap_sequences(sample['vocals']['mg'], context_length, sequence_length, batch_size)
+
+    return sample
+
+def overlap_sequences(spectrum, context_length, sequence_length, batch_size):
+    """
+        Make spectrum overlap by context length frames. return the transformed spectrum
+    """
+
+    overlap = context_length*2
+
+    trim_frame = spectrum.shape[0] % (sequence_length - overlap)
+    trim_frame -= sequence_length - overlap
     trim_frame = np.abs(trim_frame)
 
     if trim_frame != 0:
-        sample["mix"]["mg"] = np.pad(
-            sample["mix"]["mg"],
-            ((0, trim_frame), (0, 0)),
-            "constant",
-            constant_values=(0, 0),
-        )
-        sample["vocals"]["mg"] = np.pad(
-            sample["vocals"]["mg"],
+        spectrum = np.pad(
+            spectrum,
             ((0, trim_frame), (0, 0)),
             "constant",
             constant_values=(0, 0),
         )
 
-    sample["mix"]["mg"] = stride_tricks.as_strided(
-        sample["mix"]["mg"],
+    spectrum = stride_tricks.as_strided(
+        spectrum,
         shape=(
-            int(sample["mix"]["mg"].shape[0] / (sequence_length - context_length)),
+            int(spectrum.shape[0] / (sequence_length - overlap)),
             sequence_length,
-            sample["mix"]["mg"].shape[1],
+            spectrum.shape[1],
         ),
         strides=(
-            sample["mix"]["mg"].strides[0] * (sequence_length - context_length),
-            sample["mix"]["mg"].strides[0],
-            sample["mix"]["mg"].strides[1],
+            spectrum.strides[0] * (sequence_length - overlap),
+            spectrum.strides[0],
+            spectrum.strides[1],
         ),
     )
-    sample["mix"]["mg"] = sample["mix"]["mg"][:-1, :, :]
+    spectrum = spectrum[:-1, :, :]
 
-    sample["vocals"]["mg"] = stride_tricks.as_strided(
-        sample["vocals"]["mg"],
-        shape=(
-            int(sample["vocals"]["mg"].shape[0] / (sequence_length - context_length)),
-            sequence_length,
-            sample["vocals"]["mg"].shape[1],
-        ),
-        strides=(
-            sample["vocals"]["mg"].strides[0] * (sequence_length - context_length),
-            sample["vocals"]["mg"].strides[0],
-            sample["vocals"]["mg"].strides[1],
-        ),
-    )
-    sample["vocals"]["mg"] = sample["vocals"]["mg"][:-1, :, :]
-
-    b_trim_frame = sample["mix"]["mg"].shape[0] % batch_size
+    b_trim_frame = spectrum.shape[0] % batch_size
     if b_trim_frame != 0:
-        sample["mix"]["mg"] = sample["mix"]["mg"][:-b_trim_frame, :, :]
-        sample["vocals"]["mg"] = sample["vocals"]["mg"][:-b_trim_frame, :, :]
+        spectrum = spectrum[:-b_trim_frame, :, :]
 
-    sample["mix"]["mg"] = torch.clamp(torch.from_numpy(sample["mix"]["mg"]), 0, 1)
-    sample["vocals"]["mg"] = torch.clamp(torch.from_numpy(sample["vocals"]["mg"]), 0, 1)
-
-    return sample
+    return spectrum
